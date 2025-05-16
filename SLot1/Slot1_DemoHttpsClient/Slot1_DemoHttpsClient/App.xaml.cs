d93e7b55d7635837da9b2e3d51c82ba8e6f50ac0
Zang <giangpthe171781@fpt.edu.vn>
2025-05-16 16:15:41 +0700

    end slot1

---
+++ SLot1/Slot1_DemoHttpsClient/Slot1_DemoHttpsClient/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Slot1_DemoHttpsClient
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
